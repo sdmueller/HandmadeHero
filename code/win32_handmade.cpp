b0b6dc6eb5956a42d1e4c9350b2b7ca1c43704ab
Simon Müller <simon.mueller@simondo.ch>
2016-02-27 14:32:37 +0100

    Day004 - 40:00

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,54 +1,95 @@
 #include <windows.h>
+#include <stdint.h>
 
 #define internal static
 #define local_persist static
 #define global_variable static
 
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
 // TODO This is a global for now
 global_variable bool Running; //automatically initialised to 0
+
 global_variable BITMAPINFO BitmapInfo;
 global_variable void *BitmapMemory;
-global_variable HBITMAP BitmapHandle;
-global_variable HDC BitmapDeviceContext;
+global_variable int BitmapWidth;
+global_variable int BitmapHeight;
 
 internal void
 Win32ResizeDIBSection(int Width, int Height)
 {
     // TODO Bulletproof this
 
-    if(BitmapHandle)
+    if(BitmapMemory)
     {
-        DeleteObject(BitmapHandle);
+        VirtualFree(BitmapMemory, 0, MEM_RELEASE);
     }
 
-    if(!BitmapDeviceContext)
-    {
-        // TODO Should we recreate these under certain special circumstances
-        BitmapDeviceContext = CreateCompatibleDC(0);
-    }
+    BitmapWidth = Width;
+    BitmapHeight = Height;
 
     BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
-    BitmapInfo.bmiHeader.biWidth = Width;
-    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+    BitmapInfo.bmiHeader.biHeight = -BitmapHeight;
     BitmapInfo.bmiHeader.biPlanes = 1;
     BitmapInfo.bmiHeader.biBitCount = 32;
     BitmapInfo.bmiHeader.biCompression = BI_RGB;
 
+    int BytesPerPixel = 4;
+    int BitmapMemorySize = (Width * Height) * BytesPerPixel;
+    BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
 
-    BitmapHandle = CreateDIBSection(
-        BitmapDeviceContext, &BitmapInfo,
-        DIB_RGB_COLORS,
-        &BitmapMemory,
-        0, 0);
+    int Pitch = Width * BytesPerPixel;
+    uint8 *Row = (uint8 *)BitmapMemory;
+    for(int Y = 0; Y < BitmapHeight; ++Y)
+    {
+        uint8 *Pixel = (uint8 *)Row;
+        for(int X = 0; X < BitmapWidth; ++X)
+        {
+            /* Paint the screen
+             Pixel in memory: BB GG RR xx
+             LITTLE ENDIAN ARCHITECTURE!
+             Value in register:
+             0x xxRRGGBB
+            */
+            // Blue
+            *Pixel = (uint8)X;
+            ++Pixel;
+            // Green
+            *Pixel = (uint8)Y;
+            ++Pixel;
+            // Red
+            *Pixel = 0;
+            ++Pixel;
+            // Padding
+            *Pixel = 0;
+            ++Pixel;
+        }
 
+        Row += Pitch;
+    }
 }
 
 internal void
-Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+Win32UpdateWindow(HDC DeviceContext, RECT *WindowRect, int X, int Y, int Width, int Height)
 {
+    int WindowWidth = WindowRect->right - WindowRect->left;
+    int WindowHeight = WindowRect->bottom - WindowRect->top;
     StretchDIBits(  DeviceContext,
+                    /*
                     X, Y, Width, Height,
                     X, Y, Width, Height,
+                    */
+                    0, 0, BitmapWidth, BitmapHeight,
+                    0, 0, WindowWidth, WindowHeight,
                     BitmapMemory,
                     &BitmapInfo,
                     DIB_RGB_COLORS, SRCCOPY);
@@ -98,7 +139,11 @@ Win32MainWindowCallback( HWND   Window,
             int Y = Paint.rcPaint.top;
             int Width = Paint.rcPaint.right - Paint.rcPaint.left;
             int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-            Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
+
+            RECT ClientRect;
+            GetClientRect(Window, &ClientRect);
+
+            Win32UpdateWindow(DeviceContext, &ClientRect, X, Y, Width, Height);
             EndPaint(Window, &Paint);
         } break;
 
@@ -175,3 +220,4 @@ WinMain(HINSTANCE Instance,
 
     return(0);
 }
+//40:00 Day004
